a2a28345ff731a1a8a256b7072a1a0d020210061
Rajan1851421 <rajanprajapati743@gmail.com>
2024-11-21 18:04:25 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Signup from './components/signup.jsx';
+import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx';
 import Dashboard from './components/dashboard/Dashboard.jsx';
 import UploadVideo from './components/dashboard/UploadVideo.jsx'
@@ -12,20 +12,22 @@ import History from './components/dashboard/History.jsx';
 function App() {
   const myRoutes = createBrowserRouter([
     { path: '/', element: <Login /> },
-    {path:'/upload', element:<UploadVideo/>},
-    { path: '/dashboard', element: <Dashboard />,children:[
-      {path:'home', element :<Home/> },
-      {path:'upload',element:<UploadVideo/>},
-      {path:'my-videos', element:<MyVideos/> },
-      {path:'login', element: <Login/> },
-      {path:'signup', element:<Signup/>},
-      {path:'history' ,element: <History/> }
-    ] },
+    { path: '/upload', element: <UploadVideo /> },
+    {
+      path: '/dashboard', element: <Dashboard />, children: [
+        { path: 'home', element: <Home /> },
+        { path: 'upload', element: <UploadVideo /> },
+        { path: 'my-videos', element: <MyVideos /> },
+        { path: 'login', element: <Login /> },
+        { path: 'signup', element: <Signup /> },
+        { path: 'history', element: <History /> }
+      ]
+    },
   ]);
 
   return (
     <div>
-  
+
       <RouterProvider router={myRoutes}></RouterProvider>
     </div>
   );
